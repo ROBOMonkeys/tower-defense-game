682c040fbabd91a55d322d03d41d5944d64a9ede
Zac <zactepps@gmail.com>
2015-09-23 02:57:52 -0400

    made menu/button testing more robust by allowing user passage of option: python2 main.py menu|buttons fixed import error (ui.menus -> ui.menu)

--- main.py
+++ main.py
@@ -3,9 +3,10 @@
 # TODO: grid based map system
 
 import pygame
+from sys import argv
 from pygame.locals import QUIT, KEYDOWN, MOUSEBUTTONDOWN, K_q
 from ui.button import ImageButton, TextButton
-#from ui.menus import Menu
+from ui.menu import Menu
 from ui.enums import RESOURCES
 
 
@@ -37,20 +38,22 @@ screen.blit(bg, (0, 0))
 screen.blit(monkey, (300, 300))
 pygame.display.update()
 
-buttons = [ImageButton(RESOURCES + 'ok_button.png', (300, 200), test),
-           TextButton("Start", (300, 100), (0, 0, 0), callback=test)]
-
-for btn in buttons:
-    btn.draw(screen)
-
-#mn1 = Menu(("New Title", (300, 200)),
-#           ("Start", "Options", "Quit"),
-#           ((310, 300),
-#            (310, 340),
-#            (310, 380)),
-#           (start_game, open_options, quit_game))
-
-#mn1.draw(screen)
+if argv[1] == "buttons":
+    buttons = [ImageButton(RESOURCES + 'ok_button.png', (300, 200), test),
+               TextButton("Start", (300, 100), (0, 0, 0), callback=test)]
+    
+    for btn in buttons:
+        btn.draw(screen)
+elif argv[1] == "menu":
+    
+    mn1 = Menu(("New Title", (300, 200)),
+               ("Start", "Options", "Quit"),
+               ((310, 300),
+                (310, 340),
+                (310, 380)),
+               (start_game, open_options, quit_game))
+    
+    mn1.draw(screen)
 
 while running:
     for event in pygame.event.get():
@@ -59,9 +62,11 @@ while running:
         elif event.type == KEYDOWN and event.key == K_q:
             running = False
         elif event.type == MOUSEBUTTONDOWN:
-#            mn1.handle_clicks()
-            for btn in buttons:
-                btn.click()
+            if buttons:
+                for btn in buttons:
+                    btn.click()
+            else:
+                mn1.handle_clicks()
     pygame.display.update()
 
 pygame.quit()
