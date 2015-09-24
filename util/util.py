f7408854ccc9424326869c6c8a398728dcad87a0
Zachary Epps <zactepps@gmail.com>
2015-09-24 15:31:01 -0400

    added function to add all path files added into add_map and add_path to load images if passed strings

--- util/util.py
+++ util/util.py
@@ -1,18 +1,29 @@
 import ui.enums as enums
+from pygame import image
+from os import listdir
 
 def add_path(new_path):
     if type(new_path) == tuple:
         for path in new_path:
+            if type(new_path) == str:
+                path = image.load(path).convert_alpha()
             enums.PATHS.append(path)
     else:
         enums.PATHS.append(new_path)
 
+def add_all_paths():
+    for file in listdir(enums.RESOURCES):
+        if file.split(".")[0][:-1] == "path":
+            add_path(enums.RESOURCES + file)
+        
 def set_current_map(new_map):
     enums.CUR_MAP = new_map
 
 def add_map(new_map):
     if type(new_map) == tuple:
         for nmap in new_map:
+            if type(new_path) == str:
+                path = image.load(path).convert_alpha()
             enums.MAPS.append(nmap)
     else:
         enums.MAPS.append(new_map)
